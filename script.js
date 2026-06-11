c17ac7ccf6c2434fe1218eadfea007c8939dd269
ConnorPia <connorquinn@berkeley.edu>
2026-06-11 10:56:12 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY29ubm9ycGlhIiwiYSI6ImNtcThncW56eDAybWYycm9keHB0cWQwOHgifQ.wyaMP4dsFjEfFL7s0i8YRw';
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/connorpia/cmq96pt92004e01sq55j3bu5v', // your Style URL goes here
+  center: [-122.27, 37.87], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  zoom: 9 // starting zoom
+    });
+
