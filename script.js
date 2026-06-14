26b96f19c502c20a2eb50430cdde8cc3f93bda19
ConnorPia <connorquinn@berkeley.edu>
2026-06-14 03:37:02 -0700

    Change map center coordinates

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY29ubm9ycGlhIiwiYSI6ImNtcThncW56eDAybWYycm9ke
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/connorpia/cmq96pt92004e01sq55j3bu5v', // your Style URL goes here
-  center: [-122.27, 37.87], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  center: [-116.58, 32.95], // starting position [lng, lat]. Note that lat must be set between -90 and 90
   zoom: 9 // starting zoom
     });
 map.on('load', function () {
