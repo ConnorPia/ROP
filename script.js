172c451f747c8c299b1b9c07638ee3db02f4fd47
ConnorPia <connorquinn@berkeley.edu>
2026-06-14 04:35:07 -0700

    Change Mapbox style URL to custom style

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiY29ubm9ycGlhIiwiYSI6ImNtcThncW56eDAybWYycm9keHB0cWQwOHgifQ.wyaMP4dsFjEfFL7s0i8YRw';
 const map = new mapboxgl.Map({
   container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/streets-v12', // your Style URL goes here
+  style: 'mapbox://styles/connorpia/cmqdouab9008a01sm2u8e1cmi', // your Style URL goes here
   center: [-116.58, 32.95], // starting position [lng, lat]. Note that lat must be set between -90 and 90
   zoom: 11 // starting zoom
     });
